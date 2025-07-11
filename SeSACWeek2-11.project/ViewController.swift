5b8f28ddf49f01d9836e358e26aba6df01103678
etn-muguet <nmnm000@kakao.com>
2025-07-11 16:48:07 +0900

    Initial Commit

---
+++ SeSACWeek2-11.project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek2-11.project
+//
+//  Created by 김현지 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
