182a5ba6e7edc775b0f422e52553196d0cad4506
etn-muguet <nmnm000@kakao.com>
2025-07-11 18:07:39 +0900

    디자인 및 구조체 활용한 코드작성

---
+++ SeSACWeek2-11.project/shoppingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  shoppingTableViewCell.swift
+//  SeSACWeek2-11.project
+//
+//  Created by 김현지 on 7/11/25.
+//
+
+import UIKit
+
+class shoppingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
